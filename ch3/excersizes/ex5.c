39c0d7a02aeeaf57c60f47a2426763f00409d8bc
Michael Lu <yeskky@gmail.com>
2020-12-17 14:41:06 +0800

    第3章练习题

---
+++ ch3/excersizes/ex5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    float x;
+    int i;
+    float y;
+    scanf("%f%d%f",&x,&i,&y);
+    printf("x=%f\n", x);
+    printf("i=%d\n", i);
+    printf("y=%f\n", y);
+
+    return 0;
+}
